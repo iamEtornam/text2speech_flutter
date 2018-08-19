e07d621ef53bb5255903d84dcc4d309ff6a988cc
RegNex <sunumacbright@gmail.com>
2018-08-19 18:28:07 +0000

    build interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:text2speech_flutter/home.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
-      home: null,
+      home: HomePage(),
     );
   }
 }
