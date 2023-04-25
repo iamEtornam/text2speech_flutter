6b436df7563146a5f21763c74c8459763494b2a1
Etornam <sunumacbright@gmail.com>
2023-04-25 10:21:18 +0000

    theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,15 @@ import 'package:text2speech_flutter/home.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Generated App',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
         primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
         canvasColor: const Color(0xFFfafafa),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(secondary: const Color(0xFF2196f3)),
       ),
       home: HomePage(),
     );
